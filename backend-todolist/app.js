bf1d292d24a491739ee7a2fdffe2a87ad74a0068
Fransil <107945917+Fransi1@users.noreply.github.com>
2024-12-26 09:37:57 -0300

    Add files via upload

--- backend-todolist/app.js
+++ backend-todolist/app.js
@@ -1,25 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const todoRoutes = require('./routes/todoRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/todolist', {
+mongoose.connect('mongodb://127.0.0.1:27017/todolist', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('Conectado a MongoDB')).catch(err => console.error(err));
+})
+  .then(() => console.log('Conexión exitosa a MongoDB'))
+  .catch((err) => console.error('Error al conectar a MongoDB:', err));
 
-app.use('/todos', todoRoutes);
-app.get('/', (req, res) => {
-    res.send('Bienvenido a la API de Todo List, ve a http://localhost:3000/todos');
-  });
+app.use('/api', todoRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:5000/api/tasks/`);
 });
